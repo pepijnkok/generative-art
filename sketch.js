b372a22a51af149ffe997eb724b2a37b0b63f726
Pepijn Kok <pepijnkok66@gmail.com>
2021-04-15 13:22:27 +0200

    random test

--- sketch.js
+++ sketch.js
@@ -5,8 +5,23 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  ellipse(width / 2, height / 2, 50, 50);
+  background(0);
+
+  for(let i=50; i <mouseX; i+=50) {
+    for(let j=50; j <mouseY; j+=50) {
+      let r = random(2);
+      noFill()
+      stroke(mouseX-i,i,mouseY-j)
+      strokeWeight(r * 2)
+      rect(i,j, 50,50);
+    }
+  }
+
+}
+
+for (let i = 0; i < 100; i++) {
+  let r = random(-50, 50);
+  line(50, i, 50 + r, i);
 }
 
 function windowResized() {
