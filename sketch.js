249383bda7d26571facd7d9807b1ba9744029f1d
Pepijn Kok <pepijnkok66@gmail.com>
2021-05-19 22:46:26 +0200

    cleaning the code and adding comments

--- sketch.js
+++ sketch.js
@@ -6,32 +6,34 @@ function preload(){
   img = loadImage('images/gogh.jpeg');
 }
 
-//only runs once
+//Setup only runs once
 function setup() {
 createCanvas(750, 500);
 
-//making a slider
+//Making a slider for the points of the image
 sliderPoint = createSlider(0, 20, 0);
 sliderPoint.position(800, 585);
-sliderPoint.style('width', '200px');
 
+sliderPoint.style('width', '200px');
 sliderPoint.style ('z-index: +10')
 
+//Making a slider for the background of the image
 sliderBackground = createSlider(0, 255, 255);
 sliderBackground.position(450, 585);
-sliderBackground.style('width', '200px');
 
+sliderBackground.style('width', '200px');
 sliderBackground.style ('z-index: +10')
 }
 
-// loops endless
+//Draw loops endlessly
 function draw() {
 
+  //Assigning the sliders 
   let valPoint = sliderPoint.value();
   let valBackground = sliderBackground.value();
   background(valBackground);
 
-  // acces pixel information of image
+  //Acces pixel information of the image
   for(let col = 0; col< img.width; col+=10){
     for(let row = 0; row <img.height; row+=10){
       let xPos = col;
@@ -45,6 +47,7 @@ function draw() {
   }
 }
 
+//If you press 'S' you will save the image as a .jpg
 function keyPressed(){
   if (key === 's'){
     saveCanvas("art.jpg");
